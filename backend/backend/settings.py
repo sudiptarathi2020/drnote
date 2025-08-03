0fb89743fd15173afb27bc3d9c57c6577cee50ec
sudiptarathi2020 <rathisudipta99@gmail.com>
2025-08-04 02:55:37 +0600

    Add docker-compose

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,8 +96,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': '5432',
+        'ATOMIC_REQUESTS': True,
+        'CONN_MAX_AGE': 600,
     }
 }
 
